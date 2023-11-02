6e18eddb1206aa307e8cf98bc28458278d681d2b
jvcss <jvcs.mecatronica@gmail.com>
2023-11-02 15:00:02 -0300

    base for TEMPLATE

---
+++ smart_food/lib/src/screens/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Sign-In'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Sign in with Google'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ smart_food/lib/src/services/google_sign_in_service.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInService {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await _googleSignIn.signIn();
+      if (googleSignInAccount == null) {
+        return null; // User canceled the sign-in
+      }
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+      final UserCredential authResult =
+          await _auth.signInWithCredential(credential);
+      return authResult.user;
+    } catch (e) {
+      //print('Google Sign-In Error: $e');
+      return null;
+    }
+  }
+}
