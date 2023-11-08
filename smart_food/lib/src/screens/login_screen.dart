39d4942f02a5af074a608ab358c94dea3826ccf0
jvcss <jvcs.mecatronica@gmail.com>
2023-11-08 20:29:20 -0300

    refactor: login screen with function

--- smart_food/lib/src/screens/login_screen.dart
+++ smart_food/lib/src/screens/login_screen.dart
@@ -7,6 +7,13 @@ import 'package:smart_food/src/blocs/authentication/authentication_event.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
+  Future<void> _handleSignIn(BuildContext context) async {
+    // Dispatch the authentication event
+    context
+        .read<AuthenticationBloc>()
+        .add(AuthenticationSignInWithGoogleEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +25,7 @@ class LoginScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-              onPressed: () {
-                context
-                    .read<AuthenticationBloc>()
-                    .add(AuthenticationSignInWithGoogleEvent());
-              },
+              onPressed: () => _handleSignIn(context),
               child: const Text('Sign in with Google'),
             ),
           ],
