97ed4a101a0e9c3fb11189c514b19a41e646e86c
jvcss <jvcs.mecatronica@gmail.com>
2023-11-12 14:39:38 -0300

    fix: models for backend

--- smart_feeder/feeder_api/api_app/models.py
+++ smart_feeder/feeder_api/api_app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=255)
+    restaurant_name = models.CharField(max_length=255)
+    restaurant_location = models.CharField(max_length=255)
     restaurant_type = models.CharField(max_length=255)
-    ingredients = models.ManyToManyField('Ingredient', through='IngredientUsage')
+    restaurant_ingredients = models.ManyToManyField('Ingredient', through='IngredientUsage')
 
     def __str__(self):
         return self.name
--- smart_feeder/feeder_api/api_app/views.py
+++ smart_feeder/feeder_api/api_app/views.py
@@ -15,24 +15,7 @@ class RestaurantViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # Your custom logic here, to save properly the names, remove special chars
-        restaurant = serializer.save()
-
-        #api = GPTApi(api_key=config('OPENAI_API_KEY'))
-        api = GPTApi(api_key='not needed for a local LLM')
-
-        prompt = api.generate_prompt(restaurant_type=restaurant.restaurant_type)
-
-        #gpt_response = api.call_gpt_api(prompt)
-        #gpt_response = api.offline_call_gpt_028_api(prompt)
-
-        req = api.reqest_offline_gpt_028_api(prompt)
-
-        #clean_response = api.process_gpt_response(gpt_response, as_type=list)
-
-        # Add ingredients to the restaurant
-        #restaurant.ingredients.add(*clean_response)
-
+        serializer.save()
         headers = self.get_success_headers(serializer.data)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
@@ -44,3 +27,15 @@ class IngredientViewSet(viewsets.ModelViewSet):
 class UsageRateViewSet(viewsets.ModelViewSet):
     queryset = UsageRate.objects.all()
     serializer_class = UsageRateSerializer
+
+
+
+#api = GPTApi(api_key=config('OPENAI_API_KEY'))
+# api = GPTApi(api_key='not needed for a local LLM')
+# prompt = api.generate_prompt(restaurant_type=restaurant.restaurant_type)
+#gpt_response = api.call_gpt_api(prompt)
+#gpt_response = api.offline_call_gpt_028_api(prompt)
+# req = api.reqest_offline_gpt_028_api(prompt)
+#clean_response = api.process_gpt_response(gpt_response, as_type=list)
+# Add ingredients to the restaurant
+#restaurant.ingredients.add(*clean_response)
